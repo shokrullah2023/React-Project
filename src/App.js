4884adf05fb793a55a60ced2592d8c82e456ae12
shokrullah2023 <shokrullahakhlaqi01@gmail.com>
2023-10-24 22:53:19 -0600

    It's my new project for little lemon with Empty Components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+function Nav() {
+  return (
+    <nav>
+      <li>
+        <a href="/">HOME</a>
+      </li>
+      <li>
+        <a href="/">ABOUT</a>
+      </li>
+      <li>
+        <a href="/">MENU</a>
+      </li>
+      <li>
+        <a href="/">RESERVATION</a>
+      </li>
+      <li>
+        <a href="/">ORDER ONLINE</a>
+      </li>
+      <li>
+        <a href="/">LOGIN</a>
+      </li>
+    </nav>
+  );
+}
+
+export default Nav;
