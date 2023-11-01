ec308c510bdf07f408dc225b98ca1e20ee0c1646
shokrullah2023 <shokrullahakhlaqi01@gmail.com>
2023-10-31 23:39:01 -0600

    Router for navigation is added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,46 +2,69 @@ import logo from "../logo.svg";
 import "../styles/Nav.css";
 import "../styles/Queries.css";
 import BookingPage from "./BookingPage";
-import { Switch, Link, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  BrowserRouter as Link,
+  Router,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 function Nav() {
   return (
     <div className="main-nav">
       <div>
         <header>
-          {/* <meta
-            name="description"
-            content="Restaurant websit that represent Little Lemon"
-          />
-          <meta name="og:title" content="" />
-          <meta name="og:image" content="../images.png" /> */}
           <img src={logo} alt="logo-Pic" />
         </header>
       </div>
-      <div>
-        <Router>
+      <Router>
+        <div>
           <nav className="nav-list">
             <ul>
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/menu">Menu</Link>
               <li>
-                <Link to="/BookingPage">Reservation</Link>
+                <Link className="nav-elem" to="/">
+                  Home
+                </Link>
               </li>
-              <Link to="/order online">Order Online</Link>
-              <Link to="/login">Login</Link>
+              <li>
+                <Link className="nav-elem" to="/about">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-elem" to="/menu">
+                  Menu
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-elem" to="/BookingPage">
+                  Reservation
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-elem" to="/orderOnline">
+                  Order Online
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-elem" to="/login">
+                  Login
+                </Link>
+              </li>
             </ul>
           </nav>
-          <Switch>
-            <Route path="/">Home</Route>
-            <Route path="/about">About</Route>
-            <Route path="/menu">Menu</Route>
-            <Route path="/BookingPage" Component={BookingPage} />
-            <Route path="/order online">Order Online</Route>
-            <Route path="/login">Login</Route>
-          </Switch>
-        </Router>
-      </div>
+          <Router>
+            <Switch>
+              <Route path="/">Home</Route>
+              <Route path="/about">About</Route>
+              <Route path="/menu">Menu</Route>
+              <Route path="/BookingPage" element={<BookingPage />} />
+              <Route path="/orderOnline">Order Online</Route>
+              <Route path="/login">Login</Route>
+            </Switch>
+          </Router>
+        </div>
+      </Router>
     </div>
   );
 }
