85c6e287397b33ad3288f733664288c5b3ef0e34
shokrullah2023 <shokrullahakhlaqi01@gmail.com>
2023-11-09 22:16:38 -0700

    main navigation got functionlity to go to different pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import Highlights from "./components/Highlights";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BookingPage from "./components/BookingPage";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Hero />
-      <Highlights />
-      <Footer />
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" exact>
+            <Hero />
+            <Highlights />
+            <Footer />
+          </Route>
+          <Route path="/about">About</Route>
+          <Route path="/menu">Menu</Route>
+          <Route path="/booking">
+            <BookingPage />
+          </Route>
+          <Route path="/orderOnline">Order Online</Route>
+          <Route path="/login">Login</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,7 @@
 import logo from "../logo.svg";
 import "../styles/Nav.css";
 import "../styles/Queries.css";
-import BookingPage from "./BookingPage";
-import { Switch } from "react-router-dom/cjs/react-router-dom.min";
-import {
-  BrowserRouter as Link,
-  Router,
-  Route,
-} from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
@@ -17,54 +11,42 @@ function Nav() {
           <img src={logo} alt="logo-Pic" />
         </header>
       </div>
-      <Router>
-        <div>
-          <nav className="nav-list">
-            <ul>
-              <li>
-                <Link className="nav-elem" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-elem" to="/about">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-elem" to="/menu">
-                  Menu
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-elem" to="/BookingPage">
-                  Reservation
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-elem" to="/orderOnline">
-                  Order Online
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-elem" to="/login">
-                  Login
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <Router>
-            <Switch>
-              <Route path="/">Home</Route>
-              <Route path="/about">About</Route>
-              <Route path="/menu">Menu</Route>
-              <Route path="/BookingPage" element={<BookingPage />} />
-              <Route path="/orderOnline">Order Online</Route>
-              <Route path="/login">Login</Route>
-            </Switch>
-          </Router>
-        </div>
-      </Router>
+      <div>
+        <nav className="nav-list">
+          <ul>
+            <li>
+              <Link className="nav-elem" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-elem" to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-elem" to="/menu">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-elem" to="/booking">
+                Reservation
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-elem" to="/orderOnline">
+                Order Online
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-elem" to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
